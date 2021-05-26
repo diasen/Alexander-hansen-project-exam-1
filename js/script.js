5a6982795bcb81fc6eb238c1f54541a2627b92e5
Alexander Hansen <69841861+diasen@users.noreply.github.com>
2021-05-26 15:54:09 +0200

    hamburger done

--- js/script.js
+++ js/script.js
@@ -105,3 +105,13 @@ $(document).on('ready', function () {
 		getPost(currentSlide);
 	});
 });
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.navigation__menu');
+
+hamburger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+	hamburger.classList.toggle('active');
+	navMenu.classList.toggle('active');
+}
